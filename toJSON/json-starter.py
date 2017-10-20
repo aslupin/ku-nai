545c56162fdcde4f8d15a345d0639ee2f5f78809
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-10-20 14:31:22 +0700

    Starter is done

--- toJSON/json-starter.py
+++ toJSON/json-starter.py
@@ -1,9 +1,20 @@
+def printlis(l):
+    for i in l:
+        for j in i:
+            print(j,end=' ')
+        print()
 f = open("building-name.txt",'r')
 lis = []
-count = 1
 for line in f:
-    print(line)
-    line = line.split(" (")
-    lis.append([str(count),line[0],line[1].split()[1]])
-    count += 1
-print(lis)
+    if '#' in line:
+        continue
+    line = line.split()
+    level = []
+    for i in range(int(line[2])):
+        level.append([])
+    lis.append([line[0],line[1],line[2],level])
+printlis(lis)
+f.close()
+f = open("building.json",'w')
+f.write(str(lis).replace('\'','\"'))
+f.close()
