b7d326c5b3765a185c62ad7ca68d63f2dd4885e1
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-10-20 14:07:19 +0700

    before change to new ver of build.txt

--- toJSON/json-starter.py
+++ toJSON/json-starter.py
@@ -1,10 +1,9 @@
-import sys
-f = open(sys.argv[1],'r')
+f = open("building-name.txt",'r')
 lis = []
 count = 1
 for line in f:
     print(line)
     line = line.split(" (")
-    lis.append([str(count),line[0]])
+    lis.append([str(count),line[0],line[1].split()[1]])
     count += 1
 print(lis)
